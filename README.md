e78e9e2bb1c8a2ddbf39de240b493ada7db271d7
nichoth <nichoth@gmail.com>
2021-11-17 08:29:43 -0800

    readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# page test
+Trying cloudflare
+
+https://blog.cloudflare.com/building-full-stack-with-pages/
+
--- functions/hello.js
+++ functions/hello.js
@@ -2,9 +2,10 @@
 export const onRequestGet = () => {
   return new Response("Hello, world!")
 }
- 
+
 // POST requests to /filename with a JSON-encoded body would return "Hello, <name>!"
 export const onRequestPost = async ({ request }) => {
   const { name } = await request.json()
   return new Response(`Hello, ${name}!`)
 }
+
